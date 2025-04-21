a20a28169c7944cd17561b984bb31bb10cd6b5cf
Harsh Wardhan <wardhan071@gmail.com>
2025-04-21 23:12:04 +0530

    resolved merge conflicts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { sequelize } from "../config/database.config";
-import { User } from "../models/user.model";
-import { Message } from "../models/message.model";
+import { sequelize } from "./config/database.config"
+import { User } from "./models/user.model";
+import { Message } from "./models/message.model";
 
 export const db = {
   sequelize,
