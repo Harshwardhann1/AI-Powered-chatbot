30ed3653bcaf6aefd181320f9e15ce8894a2b55b
Harsh Wardhan <wardhan071@gmail.com>
2025-04-24 21:26:47 +0530

    node mailer setup completed and tested

--- src/config/nodemailer.config.ts
+++ src/config/nodemailer.config.ts
@@ -1,12 +1,12 @@
 import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST || 'smtp.gmail.com',  // Example for Gmail
-  port: Number(process.env.EMAIL_PORT) || 587,       // 465 for secure SSL/TLS, 587 for TLS (recommended)
-  secure: process.env.EMAIL_SECURE === 'true',       // true if using port 465
+  host: process.env.EMAIL_HOST || 'smtp.gmail.com', // Example for Gmail
+  port: Number(process.env.EMAIL_PORT) || 587, // 465 for secure SSL/TLS, 587 for TLS (recommended)
+  secure: process.env.EMAIL_SECURE === 'false', // true if using port 465
   auth: {
-    user: process.env.EMAIL_USER,                   // Your email address
-    pass: process.env.EMAIL_PASSWORD,               // App password (not your email login password!)
+    user: process.env.EMAIL_USER, // Your email address
+    pass: process.env.EMAIL_PASSWORD, // App password (not your email login password!)
   },
 });
 
